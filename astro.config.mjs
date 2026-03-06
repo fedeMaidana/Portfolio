9ab84cfa601f2eab719bcba8472b1a926ac1a0f4
Federico Maidana <fede_maidana4@hotmail.com>
2026-03-06 00:53:40 -0300

    Feat: add domain in astro.config.mjs for the sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: process.env.URL || 'http://localhost:4321',
+    site: 'https://maidana.dev',
     integrations: [sitemap()],
     vite: {
         css: {
